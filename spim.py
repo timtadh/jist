a8e4f9103880342a5e3063f1dabdd9cfdb86b940
Tim Henderson <tim.tadh@gmail.com>
2009-02-02 15:47:28 -0500

    spim.py is now tested.
    Tim Henderson <tim.tadh@gmail.com>

--- spim.py
+++ spim.py
@@ -9,7 +9,7 @@ should be the last thing in the last file.
 import sys
 import subprocess
 
-if __name___ != '__main__': sys.exit(0)
+if __name__ != '__main__': sys.exit(0)
 
 if len(sys.argv) < 2:
     print "Usage: python spim.py file1.s file2.s file3.s ... fileN.s"
@@ -23,7 +23,7 @@ for filename in filenames:
     t = '#'*12 + ' ' + filename + ' ' + '#'*12 + '\n'
     t += f.read()
     t += '#'*12 + ' ' + filename + ' ' + '#'*12 + '\n'
-    files.append()
+    files.append(t)
     f.close()
 
 s = '\n\n'.join(files)
@@ -32,4 +32,4 @@ f = open('__spim_py_out', 'w')
 f.write(s)
 f.close()
 
-print subprocess.check_call(["spim", "-ne",  "-mio",  "__spim_py_out"])
+subprocess.check_call(["spim", "-ne",  "-mio",  "__spim_py_out"])
