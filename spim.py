ca736713084c0951e33ecc41ffd011460cb8a882
Stephen Johnson <srj15@case.edu>
2009-02-05 16:24:48 -0500

    Changed spim.py to use build folder.
    Steve Johnson <srj15@case.edu>

--- spim.py
+++ spim.py
@@ -6,7 +6,7 @@ This is meant to be used to build a full mips program from multiple file sources
 should be the last thing in the last file.
 '''
 
-import sys
+import sys, os, shutil
 import subprocess
 import string
 import macro
@@ -47,13 +47,18 @@ def process_labels(path_in, path_out):
         s = s.replace(repl[0], repl[1])
     f2.write(s)
 
+if os.path.exists('build'): 
+    for f in os.listdir('build'):
+        os.remove(os.path.join('build', f))
+    os.rmdir('build')
+    os.makedirs('build')
+
 for filename in filenames:
+    new_path = os.path.join('build', filename)
     if filename not in label_replace_exclude:
-        new_name = '__spim_py_'+filename
-        process_labels(filename, new_name)
-        filenames_processed.append(new_name)
-    else:
-        filenames_processed.append(filename)
+        process_labels(filename, new_path)
+    shutil.copy(filename, new_path)
+    filenames_processed.append(new_path)
 
 for filename in filenames_processed:
     f = open(filename, 'r')
@@ -64,10 +69,11 @@ for filename in filenames_processed:
     f.close()
 
 s = '\n\n'.join(files)
-f = open('__spim_py_out', 'r+')
+out_path = os.path.join('build', '__spim_py_out')
+f = open(out_path, 'w')
 f.write(s)
 f.close()
 
-macro.process('__spim_py_out', '__spim_py_out')
+macro.process(out_path, out_path)
 
-subprocess.check_call(["spim", "-ne",  "-mio",  "__spim_py_out"])
+subprocess.check_call(["spim", "-ne",  "-mio",  out_path])
