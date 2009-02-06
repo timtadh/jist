3ab20dd76d101beb046de351f178ce72316e32b6
Stephen Johnson <srj15@case.edu>
2009-02-05 20:06:12 -0500

    Populated stdlib.s
    Steve Johnson <srj15@case.edu>

--- stdlib.s
+++ stdlib.s
@@ -1,1 +1,11 @@
-#This is the standard include file.
+#define get_int global
+        li $v0, 5
+        syscall
+        move %1, $v0
+#end
+
+#define print_int
+        move $a0, %1
+        li $v0, 1
+        syscall
+#end
