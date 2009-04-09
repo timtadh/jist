746e7bbac7bbe781757c489eb96b8604648c8484
Daniel DeCovnick <danielhd@mbp-2530.local>
2009-04-09 16:52:52 -0400

    This is part of the last commit

--- mappedio_test.s
+++ mappedio_test.s
@@ -8,7 +8,7 @@ read_buffer:    .space 256
 ask_num:        .asciiz "Enter a number: "
 ask_str:        .asciiz "\nEnter a string: "
 ask_char:       .asciiz "Enter a character: "
-test_fmt_str:   .asciiz "n1: %d. n2: %d.\n%%%f\nYour character: %c\nYour string: %s\n"
+test_fmt_str:   .asciiz "n1: %d. n2: %x.\n%%%f\nYour character: %c\nYour string: %s\n"
 nl:          .asciiz "\n"
 .text
 .globl main
