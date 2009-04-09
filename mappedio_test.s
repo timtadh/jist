566056e5a34d6a73499a828bc4209267d3ce4259
Stephen Johnson <srj15@case.edu>
2009-04-08 22:42:52 -0400

    Wrote buffered input and output into mappedio_test
    Steve Johnson <srj15@case.edu>

--- mappedio_test.s
+++ mappedio_test.s
@@ -14,9 +14,19 @@ main:
 read_again:
     call read_char
     add $a0 $zero $v0
-    add $s1 $zero $v0
+    sw $v0 0($s1)
+    addi $s1 $s1 4
     call write_char # Echo... echo... echo... echo...
     bne $v0 $s0 read_again
-end_read:
+    sw $zero 0($s1)
+    
+    la $s1 read_buffer
+write_again:
+    lw $a0 0($s1)
+    addi $s1 $s1 4
+    beqz $a0 end_write
+    call write_char # Echo... echo... echo... echo...
+    b write_again
+end_write:
     exit
 }
