8379a47dc7d5f178631df4898a09ae6ca1ea5bf1
Stephen Johnson <srj15@case.edu>
2009-04-21 11:03:45 -0400

    Linked list works, added data spots for context swap
    Steve Johnson <srj15@case.edu>

--- multitask_test.s
+++ multitask_test.s
@@ -6,34 +6,43 @@
 mtt_str_1: .asciiz "Look at me! I'm multitasking!"
 mtt_str_2: .asciiz "Look at me again! I'm still multitasking!"
 
+.text
+
 #format of a linked list:
 #   @ll: info block containing [head][greatest element number]
 #   @h: head: [value][next address]
 ll_init:    #a0 = value to init @h to
 {
-##define ll_init
-    # @size = $s0
-    # @ll = $t1
-    # @h = $t0
-    # @initval = $s1
-    # add $s1 $a0 $zero
+    @size = $s0
+    @ll = $t1
+    @h = $t0
+    @initval = $s1
+    add $s1 $a0 $zero
     
-    # li @size 2
-    # allocate_array @size @ll
-    # allocate_array @size @h
-    # sw @h 0(@ll)
-    # sw $zero 4(@ll)
-    # sw @initval 0(@h)
-    # sw $zero 4(@h)
-    # 
-    # add $v0 @h $zero
-    # add $v1 @ll $zero
-##end    
+    li @size 2
+    allocate_array @size @h
+    sw @initval 0(@h)
+    sw $zero 4(@h)
+    
+    add $v0 @h $zero
     return
 }
 
 ll_append:
 {
+    @size = $s0
+    @new = $t0
+    @h = $t1
+    @newval = $t2
+    add @h $a0 $zero
+    add @newval $a1 $zero
+    
+    li @size 2
+    allocate_array @size @new
+    sw @new 4(@h)
+    sw @newval 0(@new)
+    sw $zero 4(@new)
+    
     return
 }
 
@@ -43,8 +52,8 @@ ll_print:
     add @addr $a0 $zero
     
     print_again:
-        lw $s1 0($s0)
         beqz $s0 done_printing
+        lw $s1 0($s0)
         add $a0 $s1 $zero
         call print_int
         li $a0 10
@@ -55,16 +64,20 @@ ll_print:
     return
 }
 
-.text
 .globl main
 main:
 {
-    #li $a0 42
+    li $a0 42
     call ll_init
-    # add $a0 $v0 $zero
-    # call ll_print
-    # addi $a1 $zero 43
-    # call ll_append
+    add $a0 $v0 $zero
+    add $s0 $v0 $zero
+    call ll_print
+    add $a0 $s0 $zero
+    addi $a1 $zero 43
+    call ll_append
+    add $a0 $s0 $zero
+    call ll_print
+    exit
     
     # la $a0 mtt_str_1
     # call print
