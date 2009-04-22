e720a4b1b7c854523a72c7db44cb320f766c71ed
Daniel DeCovnick <danielhd@mbp-2530.local>
2009-04-22 00:59:35 -0400

    changed the flags for spim

--- spim.py
+++ spim.py
@@ -112,5 +112,5 @@ f.close()
 
 mpp.process(out_path, out_path)
 
-subprocess.check_call(["spim", "-ne",  "-mio",  out_path])
+subprocess.check_call(["spim", "-ne", "-mio", "-stext", "1048576", out_path])
 #subprocess.check_call(["spim", "-ne", out_path])
