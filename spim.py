5daea77d55461e0de7b10b27e8959bba642d523f
Daniel DeCovnick <danielhd@mbp-2530.local>
2009-04-23 01:02:41 -0400

    Fixed stupid merge artifact

--- spim.py
+++ spim.py
@@ -113,5 +113,4 @@ f.close()
 mpp.process(out_path, out_path)
 
 subprocess.check_call(["spim", "-ne", "-mio", "-stext", "1048576", out_path])
->>>>>>> 459243203357e8fff319332f3d1f2504e6022b7d:spim.py
 #subprocess.check_call(["spim", "-ne", out_path])
